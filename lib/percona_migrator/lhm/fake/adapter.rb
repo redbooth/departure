a2ea0d6ec7765b24669bd12e7e9987fe85b8a8e9
Pau Pérez <saulopefa@gmail.com>
2016-02-23 12:23:10 +0100

    Support default and limit plus strings

--- lib/percona_migrator/lhm/fake/adapter.rb
+++ lib/percona_migrator/lhm/fake/adapter.rb
@@ -11,8 +11,8 @@ module PerconaMigrator
         # Translates the Lhm's add_column syntax to ActiveRecord's and calls it
         # in the given migration
         def add_column(column_name, definition)
-          type = :integer
-          options = { limit: 11, default: nil }
+          type = type_from(column_name, definition)
+          options = options_from(column_name, definition)
 
           migration.add_column(table_name, column_name, type, options)
         end
@@ -20,6 +20,36 @@ module PerconaMigrator
         private
 
         attr_reader :migration, :table_name
+
+        def type_from(name, definition)
+          column(name, definition).type
+        end
+
+        # TODO: investigate
+        #
+        # Rails doesn't take into account lenght argument of INT in the
+        # definition, as an integer it will default it to 4 not an integer
+        def options_from(name, definition)
+          column = column(name, definition)
+          { limit: column.limit, default: column.default }
+        end
+
+        def default_value(definition)
+          match = /default '(\w+)'/i.match(definition)
+          match ? match[1] : nil
+        end
+
+        def column(name, definition)
+          @column ||= self.class.column_factory.new(
+            name,
+            default_value(definition),
+            definition
+          )
+        end
+
+        def self.column_factory
+          ::ActiveRecord::ConnectionAdapters::PerconaMigratorAdapter::Column
+        end
       end
     end
   end
--- spec/lib/percona_migrator/lhm/fake/adapter_spec.rb
+++ spec/lib/percona_migrator/lhm/fake/adapter_spec.rb
@@ -7,11 +7,6 @@ describe PerconaMigrator::Lhm::Fake::Adapter do
   let(:adapter) { described_class.new(migration, table_name) }
 
   describe '#add_column' do
-    let(:definition) { nil }
-    let(:column_name) { :some_id_field }
-    let(:type) { :integer }
-    let(:options) { { limit: 11, default: nil } }
-
     before do
       allow(migration).to(
         receive(:add_column).with(table_name, column_name, type, options)
@@ -20,10 +15,45 @@ describe PerconaMigrator::Lhm::Fake::Adapter do
 
     before { adapter.add_column(column_name, definition) }
 
-    it 'calls #add_column in the migration' do
-      expect(migration).to(
-        have_received(:add_column).with(table_name, column_name, type, options)
-      )
+    context 'with :integer' do
+      let(:definition) { 'INT(11) DEFAULT NULL' }
+      let(:column_name) { :some_id_field }
+      let(:type) { :integer }
+      # Add WithLimit and WithDefault contexts
+      let(:options) { { limit: 4, default: nil } }
+
+      it 'calls #add_column in the migration' do
+        expect(migration).to(
+          have_received(:add_column)
+          .with(table_name, column_name, type, options)
+        )
+      end
+    end
+
+    context 'with :string' do
+      let(:definition) { 'VARCHAR(255)' }
+      let(:column_name) { :body }
+      let(:type) { :string }
+      let(:options) { { limit: 255, default: nil } }
+
+      it 'calls #add_column in the migration' do
+        expect(migration).to(
+          have_received(:add_column)
+          .with(table_name, column_name, type, options)
+        )
+      end
+
+      context 'when a default value is specified' do
+        let(:definition) { "VARCHAR(255) DEFAULT 'foo'" }
+        let(:options) { { limit: 255, default: 'foo' } }
+
+        it 'calls #add_column in the migration' do
+          expect(migration).to(
+            have_received(:add_column)
+            .with(table_name, column_name, type, options)
+          )
+        end
+      end
     end
   end
 end
