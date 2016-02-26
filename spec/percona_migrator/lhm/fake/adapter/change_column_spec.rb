5c46c1419fa2b4d1a7c13aa865fb36c0fa5d1db8
Pau Pérez <saulopefa@gmail.com>
2016-02-26 12:26:35 +0100

    mv spec/lib/percona_migrator  spec/percona_migrator

--- spec/percona_migrator/lhm/fake/adapter/change_column_spec.rb
+++ spec/percona_migrator/lhm/fake/adapter/change_column_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'lib/percona_migrator/lhm/fake/adapter/shared_example_column_definition_method_spec'
+require 'percona_migrator/lhm/fake/adapter/shared_example_column_definition_method_spec'
 
 describe PerconaMigrator::Lhm::Fake::Adapter, '#change_column' do
   it_behaves_like 'column-definition method', :change_column
