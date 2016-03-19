87bff196ba2d4540d60225b6669fcdcfd3e69371
Pau Pérez <saulopefa@gmail.com>
2016-03-19 15:01:19 +0100

    Refactor TestDatabase, add doc and remove unneeded spec's config

--- test_database.rb
+++ test_database.rb
@@ -1,17 +1,33 @@
+# Setups the test database with the schema_migrations table that ActiveRecord
+# requires for the migrations, plus a table for the Comment model used throught
+# the tests.
+#
 class TestDatabase
+
+  # Constructor
+  #
+  # @param config [Hash]
   def initialize(config)
     @config = config
   end
 
-  # Creates the percona_migrator_test database and comments table in it
-  def create_test_database
-    %x(#{mysql_command} "DROP DATABASE IF EXISTS percona_migrator_test; CREATE DATABASE percona_migrator_test DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci")
-    %x(#{mysql_command} "USE percona_migrator_test; DROP TABLE IF EXISTS comments; CREATE TABLE comments (id int(12) NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;")
+  # Creates the test database, the schema_migrations and the comments tables.
+  # It drops any of them if they already exist
+  def setup
+    setup_test_database
+    drop_and_create_schema_migrations_table
+  end
+
+  # Creates the percona_migrator_test database and the comments table in it.
+  # Before, it drops both if they already exist
+  def setup_test_database
+    drop_and_create_test_database
+    drop_and_create_comments_table
   end
 
   # Creates the ActiveRecord's schema_migrations table required for
-  # migrations to work
-  def create_schema_migrations_table
+  # migrations to work. Before, it drops the table if it already exists
+  def drop_and_create_schema_migrations_table
     %x(#{mysql_command} "USE percona_migrator_test; DROP TABLE IF EXISTS schema_migrations; CREATE TABLE schema_migrations ( version varchar(255) COLLATE utf8_unicode_ci NOT NULL, UNIQUE KEY unique_schema_migrations (version)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci")
   end
 
@@ -19,6 +35,16 @@ class TestDatabase
 
   attr_reader :config
 
+  def drop_and_create_test_database
+    %x(#{mysql_command} "DROP DATABASE IF EXISTS percona_migrator_test; CREATE DATABASE percona_migrator_test DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;")
+  end
+
+  def drop_and_create_comments_table
+    %x(#{mysql_command} "USE percona_migrator_test; DROP TABLE IF EXISTS comments; CREATE TABLE comments ( id int(12) NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;")
+  end
+
+  # Returns the command to run the mysql client. It uses the crendentials from
+  # the provided config
   def mysql_command
     "mysql --user=#{config['username']} --password=#{config['password']} -e"
   end
