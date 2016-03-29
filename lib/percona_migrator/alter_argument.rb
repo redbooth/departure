a2b8375b38a9870d6083cf9c315cb60339e3a15d
Pau Pérez <saulopefa@gmail.com>
2016-03-29 21:02:08 +0200

    Validate alter argument at initialization

--- lib/percona_migrator/alter_argument.rb
+++ lib/percona_migrator/alter_argument.rb
@@ -1,31 +1,30 @@
 module PerconaMigrator
+  class InvalidAlterStatement < StandardError; end
 
   # Represents the '--alter' argument of Percona's pt-online-schema-change
   # See https://www.percona.com/doc/percona-toolkit/2.0/pt-online-schema-change.html
   class AlterArgument
     ALTER_TABLE_REGEX = /ALTER TABLE `(\w+)` /
 
+    attr_reader :table_name
+
     # Constructor
     #
     # @param statement [String]
+    # @raise [InvalidAlterStatement] if the statement is not an ALTER TABLE
     def initialize(statement)
       @statement = statement
+
+      match = statement.match(ALTER_TABLE_REGEX)
+      raise InvalidAlterStatement unless match
+
+      @table_name = match.captures[0]
     end
 
-    # Returns the '--alter' pt-online-schema-change argumment as a string. See
+    # Returns the '--alter' pt-online-schema-change argument as a string. See
     # https://www.percona.com/doc/percona-toolkit/2.0/pt-online-schema-change.html
     def to_s
       "--alter \"#{parsed_statement}\""
-    end
-
-    # Returns the name of the table the alter statement refers to
-    #
-    # @return [String]
-    def table_name
-      match = statement.match(ALTER_TABLE_REGEX)
-      raise StandardError, 'Invalid alter statement' unless match
-
-      match.captures[0]
     end
 
     private
