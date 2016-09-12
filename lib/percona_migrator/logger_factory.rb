4ceb06dddfefbdc744b35866c0d36514d1fe61b4
Bruno Trecenti <bruno.lopestrecenti@gmail.com>
2016-09-12 18:29:28 -0300

    Adds info message when using percona migrator

--- lib/percona_migrator/logger_factory.rb
+++ lib/percona_migrator/logger_factory.rb
@@ -7,6 +7,7 @@ module PerconaMigrator
     # @param verbose [Boolean]
     # @return [#say, #write]
     def self.build(verbose: true)
+      puts("Migrations will execute with PerconaMigrator\nfor more information visit https://github.com/redbooth/percona_migrator")
       if verbose
         PerconaMigrator::Logger.new
       else
