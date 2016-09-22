8b9f7498509bb983a26577ebece1a414693ff109
Pau Perez <saulopefa@gmail.com>
2016-09-22 13:13:07 +0200

    Add integration test for #rename_table

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -474,6 +474,20 @@ describe PerconaMigrator, integration: true do
     end
   end
 
+  context 'renaming a table' do
+    let(:version) { 24 }
+
+    it 'changes the table name' do
+      ActiveRecord::Migrator.run(direction, migration_path, version)
+      expect(tables).to include('new_comments')
+    end
+
+    it 'does not keep the old name' do
+      ActiveRecord::Migrator.run(direction, migration_path, version)
+      expect(tables).not_to include('comments')
+    end
+  end
+
   context 'when the migration failed' do
     context 'and the migration is not an alter table statement' do
       let(:version) { 8 }
