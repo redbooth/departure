e48578c4669f2cfa14e7061952fa90354f5b5573
Pau Perez <saulopefa@gmail.com>
2016-11-18 14:20:24 -0500

    Move hooking into rails out of the railtie

--- lib/percona_migrator.rb
+++ lib/percona_migrator.rb
@@ -24,4 +24,42 @@ module PerconaMigrator
     self.configuration ||= Configuration.new
     yield(configuration)
   end
+
+  # Hooks Percona Migrator into Rails migrations by replacing the configured
+  # database adapter
+  def self.load
+    ActiveRecord::Migration.class_eval do
+      alias_method :original_migrate, :migrate
+
+      # Replaces the current connection adapter with the PerconaAdapter and
+      # patches LHM, then it continues with the regular migration process.
+      #
+      # @param direction [Symbol] :up or :down
+      def migrate(direction)
+        reconnect_with_percona
+        include_foreigner if defined?(Foreigner)
+
+        ::Lhm.migration = self
+        original_migrate(direction)
+      end
+
+      # Includes the Foreigner's Mysql2Adapter implemention in
+      # PerconaMigratorAdapter to support foreign keys
+      def include_foreigner
+        Foreigner::Adapter.safe_include(
+          :PerconaMigratorAdapter,
+          Foreigner::ConnectionAdapters::Mysql2Adapter
+        )
+      end
+
+      # Make all connections in the connection pool to use PerconaAdapter
+      # instead of the current adapter.
+      def reconnect_with_percona
+        connection_config = ActiveRecord::Base.connection_config
+        ActiveRecord::Base.establish_connection(
+          connection_config.merge(adapter: 'percona')
+        )
+      end
+    end
+  end
 end
--- lib/percona_migrator/railtie.rb
+++ lib/percona_migrator/railtie.rb
@@ -15,40 +15,7 @@ module PerconaMigrator
     # regular Rails Migration DSL.
     initializer 'percona_migrator.configure_rails_initialization' do
       ActiveSupport.on_load(:active_record) do
-
-        ActiveRecord::Migration.class_eval do
-          alias_method :original_migrate, :migrate
-
-          # Replaces the current connection adapter with the PerconaAdapter and
-          # patches LHM, then it continues with the regular migration process.
-          #
-          # @param direction [Symbol] :up or :down
-          def migrate(direction)
-            reconnect_with_percona
-            include_foreigner if defined?(Foreigner)
-
-            ::Lhm.migration = self
-            original_migrate(direction)
-          end
-
-          # Includes the Foreigner's Mysql2Adapter implemention in
-          # PerconaMigratorAdapter to support foreign keys
-          def include_foreigner
-            Foreigner::Adapter.safe_include(
-              :PerconaMigratorAdapter,
-              Foreigner::ConnectionAdapters::Mysql2Adapter
-            )
-          end
-
-          # Make all connections in the connection pool to use PerconaAdapter
-          # instead of the current adapter.
-          def reconnect_with_percona
-            connection_config = ActiveRecord::Base.connection_config
-            ActiveRecord::Base.establish_connection(
-              connection_config.merge(adapter: 'percona')
-            )
-          end
-        end
+        PerconaMigrator.load
       end
     end
 
