4f0c0e305becc3c45f1c8ed315874e84713fa09b
Garett Arrowood <garettarrowood@gmail.com>
2017-08-26 09:34:33 -0400

    Pass arg in support file instead of hardcoded :comments

--- spec/support/table_methods.rb
+++ spec/support/table_methods.rb
@@ -4,12 +4,12 @@ module TableMethods
   end
 
   def unique_indexes_from(table_name)
-    indexes = indexes_from(:comments)
+    indexes = indexes_from(table_name)
     indexes.select(&:unique).map(&:name)
   end
 
   def indexes_from(table_name)
-    ActiveRecord::Base.connection.indexes(:comments)
+    ActiveRecord::Base.connection.indexes(table_name)
   end
 
   def tables
